e3b2aa96ae7a7945a6eedfe259cba1dd5e6c2d55
Matthew Samaniego <matthew.samaniego@my.jcu.edu.au>
2024-10-03 14:06:38 +1000

    Add randoms file

---
+++ prac_03/randoms.py
@@ -0,0 +1,23 @@
+# # What did you see on line 1?
+# # What was the smallest number you could have seen, what was the largest?
+
+# The numbers: 17, 10, 5, 7, 16
+# The smallest number possible is 5, the largest was 20
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# The numbers: 7, 9, 7, 5, 5
+# The smallest number possible is 3, the largest was 9
+# No. The only possible numbers are 5, 7, and 9
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+#  The numbers: 5.037573827300721, 2.6204185878188446, 4.27669689922251, 4.780391374091471, 3.1819421035743867, 5.339005352598643
+# The smallest number possible number is 2.5, the largest would be, 5.5
+
+import random
+
+print(random.randint(1,100))
